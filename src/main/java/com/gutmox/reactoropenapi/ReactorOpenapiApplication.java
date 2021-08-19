728fc9d34849319e53a094fb3625edc802f3699a
Pablo Gutierrez <gutmox@gmail.com>
2021-08-19 19:55:51 +0200

    Initial commit

---
+++ src/main/java/com/gutmox/reactoropenapi/ReactorOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.gutmox.reactoropenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorOpenapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutmox/reactoropenapi/ReactorOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutmox.reactoropenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
